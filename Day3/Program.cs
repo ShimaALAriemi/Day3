eb65a4a4ca0169135c2a843707c6e9f9680a8012
Shaima <shooomi9988@gmail.com>
2023-11-15 15:31:02 +0400

    first commit

---
+++ Day3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Day3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Even NumberProgram
+
+            Console.WriteLine("Enter a Number : ");
+            String evenNum = Console.ReadLine();
+            int numE = int.Parse(evenNum);
+            String addOrEven = (numE % 2 == 0) ? "Your Number is Even\n" : "Your Number is Odd\n";
+            Console.WriteLine(addOrEven);
+
+
+            // Odd NumberProgram
+
+            Console.WriteLine("Enter a Number : ");
+            String oddNum = Console.ReadLine();
+            int numO = int.Parse(oddNum);
+            String evenOrOdd = (numO % 3 == 0) ? "Your Number is Odd\n" : "Your Number is Even\n";
+            Console.WriteLine(evenOrOdd);
+
+
+        }
+    }
+}
